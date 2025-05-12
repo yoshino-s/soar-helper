ab811f1db38c8e599f6eabf52cff54a5f4f937b5
yoshino-s <cy-cui@outlook.com>
2025-05-12 18:42:17 +0800

    update

--- internal/handler/connect/s3.go
+++ internal/handler/connect/s3.go
@@ -26,7 +26,7 @@ func (s *S3Service) SetS3(s3 *s3.S3) {
 }
 
 func (s *S3Service) Upload(ctx context.Context, req *connect.Request[v1.UploadRequest]) (*connect.Response[v1.UploadResponse], error) {
-	url, err := s.s3.Upload(context.Background(), req.Msg.Key, req.Msg.Path, minio.PutObjectOptions{})
+	url, err := s.s3.Upload(ctx, req.Msg.Key, req.Msg.Path, minio.PutObjectOptions{})
 	if err != nil {
 		return nil, errors.Wrap(err, "upload file error")
 	}
